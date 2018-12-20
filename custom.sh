d2b4f15854017271dae8164b65163c8e94ba7e93
fourtwenty-poopdeck <42221406+fourtwenty-poopdeck@users.noreply.github.com>
2018-12-19 19:41:40 -0800

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#             #
+##           ##
+### custom ####
+### scriptz ###
+##           ##
+#             #
+
+
+##make nordVPN automatically start when i3 refreshes
+echo 'starting Nord VPN...'
+nordvpn connect
